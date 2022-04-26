b9d791cf30ed8a8b6c8627bb0c90cb5dce451924
Douglhas <doughbn@outlook.com>
2022-04-26 17:24:01 -0300

    Doug

--- app/exploreApp.js
+++ app/exploreApp.js
@@ -1,5 +1,31 @@
+function addEvents(){
 
+    let category =document.getElementById("categoryList")
+    
+    for (let i=0;i<category.children.length;i++)
+        category.children[i].addEventListener('click',multipleChecks);
 
-let postText = document.getElementById('explore-aside');
+    let time = document.getElementById('timeList');
 
+    for (let i=0;i<time.children.length;i++)
+        time.children[i].addEventListener('click',onlyCheck);
+    
+}
+
+addEvents()
+
+function multipleChecks(){
+    if (this.classList.contains('checked'))
+        this.setAttribute('class', 'unchecked');
+    else
+        this.setAttribute('class', 'checked');
+}
+
+function onlyCheck(){
+    let timeList = document.getElementById('timeList');
+    for (let i=0;i<timeList.children.length;i++)
+        timeList.children[i].setAttribute('class','unchecked');
+
+    this.setAttribute('class','checked');
+}
 
