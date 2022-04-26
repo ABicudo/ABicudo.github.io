df907aae690386000189400b3b595015b7ff46aa
Douglhas <doughbn@outlook.com>
2022-04-26 19:04:08 -0300

    Douglhas

--- app/exploreApp.js
+++ app/exploreApp.js
@@ -29,3 +29,32 @@ function onlyCheck(){
     this.setAttribute('class','checked');
 }
 
+function explore(){
+    let searchString = document.getElementById('search-input');
+
+    if (searchString.value !=""){
+
+    let category = document.querySelectorAll('#categoryList .checked');
+    let time = document.querySelector('#timeList .checked');
+
+    let categoryList =[];
+   for (let i=0;i<category.length;i++)
+        categoryList.push(category[i].innerText);
+    
+    let results = document.querySelector('.results')
+        if (results == null){
+            results = document.createElement('section');
+            results.setAttribute('class', 'explore-section results');
+        }else
+            results.innerHTML="";
+
+    results.innerHTML= `<h1>Resultados para: ${searchString.value}</h1>
+    <div class="details-search"><h4>Em: ${categoryList.join(' - ')}</h4>
+    <h4>Período: ${time.innerText}</h4></div>
+    <div class="result-topics">Resultados...</div>`;
+    searchString.value="";
+    let mainFeed = document.getElementById('search-content');
+    mainFeed.appendChild(results);
+}
+}
+
